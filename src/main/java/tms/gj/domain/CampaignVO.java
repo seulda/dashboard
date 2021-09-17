bd35e544082d4dcd7e9818d5662f987f2d590204
seulda <kim02977@naver.com>
2021-09-17 09:28:26 +0900

    VO

--- src/main/java/tms/gj/domain/CampaignVO.java
+++ src/main/java/tms/gj/domain/CampaignVO.java
@@ -4,7 +4,17 @@ import lombok.Data;
 
 @Data
 public class CampaignVO {
-	// vo 구현 sample
+
+	private int idx;					// number
+	private String category;			// 분류
+	private String campaignName;		// 공약명
+	private String location;			// 사업위치
+	private String term;				// 임기내/외
+	private String businessPeriod;		// 사업기간
+	private String department;			// 부서
+	private String section;				// 담당
+	private String manager;				// 담당자
+	private String fulfil;				// 자체점검 - 이행도
+	private String fulfilPercent;		// 자체점검 - 이행률
 	
-	private String create;		// 설명
 }
---
+++ src/main/java/tms/gj/domain/ItemVO.java
@@ -0,0 +1,14 @@
+package tms.gj.domain;
+
+import lombok.Data;
+
+@Data
+public class ItemVO {
+
+	private int idx;			// number
+	private int year;			// 연도
+	private String item;		// 인구동태 변화 요인
+	private int population;		// 인구수
+	private String dong;		// 행정구역 면/동
+	
+}
